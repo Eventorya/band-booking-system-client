9b39e3b1a4a73df4f7957f1a0f33193ecf41e23b
Neth22 <nseneviratne44@gmail.com>
2025-11-19 17:07:42 +0530

    [SCRUM-15][Forget Password] *update codes
    Signed-off-by: Neth22 <nseneviratne44@gmail.com>

--- src/app/(pages)/(auth)/forgot-password/page.tsx
+++ src/app/(pages)/(auth)/forgot-password/page.tsx
@@ -1,100 +1,81 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Image from "next/image";
-import Link from "next/link";
 
-// Import your assets (same as in SignUp)
 import ArtistBookLogo from "../../../../assets/ArtistsBookLogo.svg";
-import AuthCover from "../../../../assets/AuthCoverImage.jpg";
-import QuestionMark from "../../../../assets/QuestionMark.svg";
+import AuthCover from "../../../../assets/AuthCover.svg";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
   return (
-    <div className="relative bg-white overflow-hidden h-screen">
-      {/* Background blurred image */}
-      <Image
-        src={AuthCover}
-        className="w-[1537px] h-[1024.79px] left-0 top-[-0.90px] absolute opacity-20 mix-blend-multiply "
-        width={1537}
-        height={1025}
-        alt="Background"
-      />
+    <div>
+      <div className="flex flex-col min-h-screen">
+        <div className="flex justify-center lg:justify-start">
+          <Image
+            src={ArtistBookLogo}
+            className="w-[8.055rem] h-[3rem] mt-12 md:w-[15.33rem] md:h-[5.063rem] md:mt-20 lg:w-[12.313rem] lg:h-[3.058rem] lg:mt-[2.438rem] lg:ml-[2.438rem]"
+            alt="Artist Book Logo"
+          />
+        </div>
 
-      {/* Logo */}
-      <Image
-        src={ArtistBookLogo}
-        className="w-[200px] h-[100px] mt-2 ml-20 relative z-10"
-        alt="Artist Book Logo"
-      />
+        <div className="flex items-center justify-center flex-col px-4">
+          <h2 className="mt-[2.5rem] text-[#00654F] text-center font-[Montserrat] text-[1.563rem] font-bold line-height-[6.063rem]">
+            Forgot Password ?
+          </h2>
 
-      {/* Main Centered Card */}
-      <div className="flex justify-center items-center h-[calc(100vh-104px)] w-full">
-        <div className="rounded-2xl flex w-[800px] shadow-2xl overflow-hidden h-[400px]">
-          {/* Left Side - Form */}
-          <div className="w-[50%]">
-            <Card className="bg-white backdrop-blur-md shadow-2xl rounded-none h-[400px] border-0">
-              <CardHeader className="pt-8 pb-2">
-                <CardTitle className="text-center text-emerald-800 text-4xl font-bold font-['Montserrat'] mb-4">
-                  Forgot Password ?
-                </CardTitle>
-                <p className="text-center text-gray-600 text-sm font-poppins">
-                  Enter your email address below and we'll send <br /> you a
-                  password reset OTP
-                </p>
-              </CardHeader>
+          <div className="mt-[1.625rem] justify-center items-center">
+            <p className="text-[#000] font-[Poppins] text-[0.688rem] font-medium line-height-[1.063rem] text-center ml-[4.50rem] mr-[5.063rem]">
+              Enter your email address below and we'll send you a password reset
+              OTP
+            </p>
+          </div>
 
-              <CardContent className="px-10 mt-4">
-                <div className="space-y-4">
-                  <div className="grid gap-2">
-                    <Label
-                      htmlFor="email"
-                      className="text-black text-base font-normal font-poppins"
-                    >
-                      Email
-                    </Label>
-                    <Input
-                      id="email"
-                      type="email"
-                      placeholder="Type your email"
-                      required
-                      className="bg-zinc-100 text-stone-500 text-xs font-normal font-poppins h-12"
-                    />
-                  </div>
-                </div>
-              </CardContent>
-
-              <div className="px-10 mt-1">
-                <Button className="w-full h-12 text-base font-semibold bg-gradient-to-br from-emerald-800 to-teal-500 hover:scale-105 duration-200 ease-in-out">
-                  Send OTP
-                </Button>
-
-                <p className="text-center mt-4 text-sm font-poppins">
-                  Don't you have an account?{" "}
-                  <Link
-                    href="/signup"
-                    className="text-blue-600 underline underline-offset-4"
-                  >
-                    Sign Up
-                  </Link>
-                </p>
+          <form>
+            <div className="w-full max-w-[400px] mt-4 ">
+              <Label
+                htmlFor="email"
+                className="text-[#000] font-[Poppins] text-sm font-medium mb-2 block"
+              >
+                Email
+              </Label>
+              <div className="relative ">
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full pr-10 border  border-black/12 rounded-md h-10 text-sm bg-[#F3F3F3] font-[Poppins] line-height-[1.063rem] "
+                />
               </div>
-            </Card>
-          </div>
 
-          {/* Right Side - Big Question Mark */}
-          <div className="w-[50%] bg-white backdrop-blur-md flex items-center justify-center">
-            <Image
-              src={QuestionMark}
-              width={250}
-              height={250}
-              className="object-cover"
-              alt="Forgot Password Illustration"
-            />
-          </div>
+              <div className="mt-4 flex items-center gap-2 text-sm justify-center">
+                <p className="text-[#000] font-[Montserrat] font-normal text-[0.625rem] text-center line-hieght-[6.063rem] ">
+                  Don't You Have an Account?
+                </p>
+                <a
+                  href="#"
+                  className="text-[#1877F2] font-[Montserrat]   text-center text-[0.625rem] font-semibold line-hieght-[6.063rem] tracking-normal"
+                >
+                  Sign Up
+                </a>
+              </div>
+
+              <div className="w-full px-[6.25rem] mt-[1rem]">
+                <Button
+                  type="submit"
+                  className="bg-gradient-to-br from-emerald-800 to-teal-500 hover:scale-105 duration-200 ease-in-out w-[5.813rem] text-sm font-bold font-poppins h-[2.25rem] rounded-xl "
+                >
+                  RESET
+                </Button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
